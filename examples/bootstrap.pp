600bb17ca3191e9652a5c025c8b56f9a76be8a5d
Niklaus Giger <niklaus.giger@member.fsf.org>
2010-11-09 16:01:34 +0100

    Added doc for bootstrap

--- examples/bootstrap.pp
+++ examples/bootstrap.pp
@@ -9,33 +9,17 @@ file {"/var/git": ensure => directory, owner => git,
 	require => [User["git"], File["/var/git"]],
 }
 
-ssh_authorized_key { "git":
-	ensure => present,
-	key => "INSERT PUBLIC KEY HERE",
-	name => "git@atalanta-systems.com",
-	target => "/var/git/.ssh/authorized_keys",
-	type => rsa,
-	require => File["/var/git"],
-}
-
-yumrepo { "elexis-admin":
-	baseurl => "git://github.com/ngiger",
-	descr => "Elexis admin made easy",
-	enabled => 1,
-	gpgcheck => 0,
-	name => "elexis-admin",
-}
-
 package { "git":
 	ensure => installed,
-	require => Yumrepo["elexis-admin"],
 }
 
 exec { "Create puppet Git repo":
 	cwd => "/var/git/puppet",
 	user => "git",
-	command => "/usr/bin/git init --bare",
+	unless => "/usr/bin/git status -s",
+	command => "/usr/bin/git clone git://github.com/ngiger/elexis-admin.git .",
 	creates => "/var/git/puppet/HEAD",
 	require => [File["/var/git/puppet"], Package["git"], User["git"]],
 }
 
+# vi:syntax=puppet:filetype=puppet:ts=4:et:
--- examples/vim-puppet.pp
+++ examples/vim-puppet.pp
@@ -1,4 +1,3 @@
 package { "vim-puppet":
 	  ensure => installed,
 }
-
