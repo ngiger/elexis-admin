d625b39a1d6f82e1c699e89c92b67b55ab338f83
Niklaus Giger <niklaus.giger@member.fsf.org>
2011-12-09 18:36:51 +0100

    fixed x2go

--- modules/x2go/manifests/init.pp
+++ modules/x2go/manifests/init.pp
@@ -1,2 +1,4 @@
 # Copyright 2011 Niklaus Giger <niklaus.giger@member.fsf.org>
+class x2go {
 import "*.pp"
+}
--- modules/x2go/manifests/x2go-client.pp
+++ modules/x2go/manifests/x2go-client.pp
@@ -1,6 +1,4 @@
 class x2go::client   inherits x2go::common {
-
-
   package { "x2goclient":
     ensure => installed,
     require => Exec["x2go_apt_update"]
